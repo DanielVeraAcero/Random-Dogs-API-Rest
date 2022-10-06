f019e9928d46ac3f28ff1f25402fd0e9455227e5
Daniel Vera <d.vera.acero@gmail.com>
2022-10-06 16:51:27 -0500

    add query parameters

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-dogsAPI = 'https://api.thedogapi.com/v1/images/search'
+dogsAPI = 'https://api.thedogapi.com/v1/images/search?limit=3'
 
-const randomDogImg = document.querySelector('.randomDog')
+const randomDogImg_1 = document.querySelector('.randomDog1')
+const randomDogImg_2 = document.querySelector('.randomDog2')
+const randomDogImg_3 = document.querySelector('.randomDog3')
 const buttonNewDog = document.querySelector('.newDog')
 
 async function fetchRandomDog() {
     let response = await fetch(dogsAPI)
     let data = await response.json()
-    randomDogImg.src = data[0].url
+    console.log(data);
+    randomDogImg_1.src = data[0].url
+    randomDogImg_2.src = data[1].url
+    randomDogImg_3.src = data[2].url
 }
 
+fetchRandomDog()
 buttonNewDog.addEventListener('click',fetchRandomDog)
-
