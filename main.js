1e73c63afa95e58928efeb4555ff95f7714911b5
Daniel Vera <d.vera.acero@gmail.com>
2022-10-07 01:12:03 -0500

    add rendering dogs

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
-dogsAPI = 'https://api.thedogapi.com/v1/images/search?limit=3'
+dogsAPI = 'https://api.thedogapi.com/v1/images/search?limit=4'
 
-const randomDogImg = document.querySelector('.randomDog__img')
-const buttonNewDog = document.querySelector('.newDog')
+const randomDogContainer = document.querySelector('.randomDogs__content')
+const buttonNewDog = document.querySelector('.randomDogs__getNew')
+
+function removeChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild)
+    }
+}
 
 async function fetchRandomDog() {
     let response = await fetch(dogsAPI)
     let data = await response.json()
-    console.log(data);
-    randomDogImg.src = data[0].url
+    // console.log(data);  
+
+    data.forEach((element,index) => {
+        randomDogContainer.innerHTML += ` 
+                <article class="randomDog">
+                    <img class="randomDog__img" src="${data[index].url}" alt="">
+                    <i class="ri-heart-fill randomDog__iconFav"></i>
+                </article>
+            `;
+    });
 }
 
 fetchRandomDog()
-// buttonNewDog.addEventListener('click',fetchRandomDog)
+buttonNewDog.addEventListener('click', () => {removeChildNodes(randomDogContainer); fetchRandomDog()})
